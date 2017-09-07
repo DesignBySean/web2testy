e0d6c6ce3c0093d4597d1c32796f958639391563
DesignBySean <DesignBySean@aol.com>
2017-09-07 16:51:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ console.log('hello sean!')
 document.write('<h1> Hi!</h1>')
 
 adj_list = ["Bitter","Lemon-flavored","Spicy","Bland","Minty","Sweet","Delicious","Pickled","Tangy","Fruity","Salty"]
-(11) ["Bitter", "Lemon-flavored", "Spicy", "Bland", "Minty", "Sweet", "Delicious", "Pickled", "Tangy", "Fruity", "Salty"]
 noun_list = ["army", "navy", "airforce", "marines"]
-(4) ["army", "navy", "airforce", "marines"]
 "
 x = adj_list[1]
 
 sentence_a = "She loved the "+x+" taste of apples and loved the "+noun_list[3]+"."
 document.write(sentence_a)
-"how many "+adj_list[2]+" chicken are there?"
-"how many Spicy chicken are there?"
+
+sentence_b = "how many "+adj_list[2]+" chicken are there?"
+document.write(sentence_b)
