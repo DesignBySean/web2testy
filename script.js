fde53c469291e6bfaf6a335640fb783b910d9e2a
DesignBySean <DesignBySean@aol.com>
2017-09-07 17:03:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ x = adj_list[1]
 sentence_a = "She loved the "+x+" taste of apples and loved the "+noun_list[3]+"."
 document.write(sentence_a)
 
-sentence_b = " How many "+adj_list[2]+" chicken are there?"
+sentence_b = " I do not know why she likes "+adj_list[2]+" food."
 document.write(sentence_b)
 
-sentence_c = " She thought about joining the "+noun_list[2]+"."
+sentence_c = " Even though she loves the "+noun_list[3]+" she thought about joining the "+noun_list[2]+"."
 document.write(sentence_c)
 
 alert('AWESOME')
