9b4f23a7a215e303d9406e698df639c4a67c586c
DesignBySean <DesignBySean@aol.com>
2017-09-07 17:30:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ document.write('<h1> Hi!</h1>')
 adj_list = ["bitter","lemon-flavored","spicy","bland","minty","sweet","delicious","pickled","tangy","fruity","salty"]
 noun_list = ["army", "navy", "airforce", "marines"]
 
-x = adj_list[1]
+noun = noun_list[Math.floor(Math.random()*noun_list.length]
+adj = adj_list[Math.floor(Math.random()*adj_list.length]
 
-sentence_a = "She loved the "+x+" taste of apples and loved the "+noun_list[3]+"."
+sentence_a = "She loved the "+adj+" taste of apples and loved the "+noun+"."
 document.write(sentence_a)
 
-sentence_b = " I do not know why she likes "+adj_list[2]+" food."
+sentence_b = " I do not know why she likes "+adj+" food."
 document.write(sentence_b)
 
-sentence_c = " Even though she loves the "+noun_list[3]+" she thought about joining the "+noun_list[2]+"."
+sentence_c = " Even though she loves the "+noun+" she thought about joining the "+noun+"."
 document.write(sentence_c)
 
 alert('AWESOME')
