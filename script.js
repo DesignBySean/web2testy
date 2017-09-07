4cfa38d9fd4e143af60b65c55ba4eb90b4a7f443
DesignBySean <DesignBySean@aol.com>
2017-09-07 16:52:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.write('<h1> Hi!</h1>')
 
 adj_list = ["Bitter","Lemon-flavored","Spicy","Bland","Minty","Sweet","Delicious","Pickled","Tangy","Fruity","Salty"]
 noun_list = ["army", "navy", "airforce", "marines"]
-"
+
 x = adj_list[1]
 
 sentence_a = "She loved the "+x+" taste of apples and loved the "+noun_list[3]+"."
